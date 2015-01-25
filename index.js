bbcfa52e968d1f603ac70e556694ebbead97ec70
Eugene Sharygin <eush77@gmail.com>
2015-01-26 02:26:05 +0300

    Use npm's own module to obtain the paths

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 'use strict';
 
-var exec = require('child_process').exec
-  , path = require('path')
-  , fs = require('fs')
+var path = require('path')
   , PassThrough = require('readable-stream').PassThrough
-  , zlib = require('zlib')
-  , tar = require('tar')
+  , Packer = require('fstream-npm')
 
-var go = module.exports = 
+var localName = function (entryPath, rootPath) {
+  return entryPath.slice(path.dirname(rootPath).length + '/package/'.length);
+};
+
+var go = module.exports =
 
 /**
- * Invokes `npm pack` to determine what would be included during `npm publish`.
- * 
+ * Uses `fstream-npm` to determine what would be included during `npm publish`.
+ *
  * @name irishPub
  * @function
  * @param {string} root path to package to publish, defaults to `cwd`
@@ -21,28 +22,15 @@ function irishPub(root) {
   root = root || process.cwd();
 
   var out = new PassThrough();
-  exec('npm pack ' + root, function (err, stderr, stdout) {
-    if (err) return console.error(err);
 
-    // npm logs created filename on stderr
-    var tarFile = path.join(process.cwd(), stderr.trim()); 
-
-    fs.createReadStream(tarFile)
-      .on('error', out.emit.bind(out, 'error'))
-      .pipe(zlib.createGunzip())
-      .on('error', out.emit.bind(out, 'error'))
-      .pipe(tar.Parse())
-      .on('error', out.emit.bind(out, 'error'))
-      .on('entry', function (e) {
-        out.write(e.path.replace(/^package\//, '') + '\n');
-      })
-      .on('end', function () {
-        fs.unlink(tarFile, function (err) {
-          if (err) return out.emit(err);
-          out.emit('end')
-        })
-      })
-  })
+  Packer(root)
+    .on('error', out.emit.bind(out, 'error'))
+    .on('child', function (entry) {
+      out.write(localName(entry.path, entry.root.path) + '\n');
+    })
+    .on('end', function () {
+      out.emit('end');
+    });
 
   return out;
 }
